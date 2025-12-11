e28f40aa8ef4a9359ee2bb9399248e50ba611ed5
Skander Hawess <skander.hawess@esprit.tn>
2025-12-11 20:59:25 +0100

    Test webhook auto build

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -1,6 +1,7 @@
 package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
